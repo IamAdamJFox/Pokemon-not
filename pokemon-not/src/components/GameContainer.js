a10a209ef0982587c70666bb7c137e3df35b16f4
Shannon <shogeboom187@gmail.com>
2023-07-31 20:57:03 -0700

    making gameContainer

---
+++ pokemon-not/src/components/GameContainer.js
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react';
+
+export default function GameContainer() {
+    const [currentPage, setCurrentPage] = useState("StartMenu");
+
+    const renderPage = () => {
+        if (currentPage === "StartMenu") {
+            return <StartMenu />;
+        }
+        if (currentPage === "BuildInfo") {
+            return <BuildInfo />;
+        }
+    };
+
+    return (
+        <div>
+            <Header />
+            {renderPage()}
+        </div>
+    );
+}
