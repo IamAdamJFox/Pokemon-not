bc4ccc1942affa6dbd49727592ca18c9d64784a3
adamjfox <adamjeffersonfox@gmail.com>
2023-08-01 21:28:32 -0700

    moves

--- server/controllers/placeholder.js
+++ server/controllers/placeholder.js
@@ -36,4 +36,15 @@ fetch ('https://pokeapi.co/api/v2/pokemon/25/',{
 .then(data => console.log(data))
 .catch(error => console.log('Error:', error));
 
+fetch ('https://pokeapi.co/api/v2/move/1/',{
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+ 
+}).then(response => {return response.json()})
+.then(data => console.log(data))
+.catch(error => console.log('Error:', error));
+
+
 
