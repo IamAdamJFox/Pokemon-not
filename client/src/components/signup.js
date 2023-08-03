d6c1ab8818bef388f6d21ed3694fe161a88ca57b
adamjfox <adamjeffersonfox@gmail.com>
2023-08-02 19:34:52 -0700

    commenting out so i dont break

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -6,19 +6,19 @@ export default function SignUpAndLogin() {
     password: "",
     email: "",
   });
-
-  const Signup = () => {
-    // set initial form state
-    const [userFormData, setUserFormData] = useState({
-      username: "",
-      email: "",
-      password: "",
-    });
+//example for hieu
+  // const Signup = () => {
+  //   // set initial form state
+  //   const [userFormData, setUserFormData] = useState({
+  //     username: "",
+  //     email: "",
+  //     password: "",
+  //   });
   
-    // set state for form validation
-    const [validated] = useState(false);
-    // set state for alert
-    const [showAlert, setShowAlert] = useState(false);
+  //   // set state for form validation
+  //   const [validated] = useState(false);
+  //   // set state for alert
+  //   const [showAlert, setShowAlert] = useState(false);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
