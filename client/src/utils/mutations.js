a1877baa391024aa4f29be0055b054f5671922d0
David Flores <dcflores@live.com>
2023-08-03 21:13:13 -0700

    added login and add user mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,29 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 export const CREATE_POKEMON = gql`
   mutation createPokemon($name: String!, $type: String!) {
     createPokemon(name: $name, type: $type) {
